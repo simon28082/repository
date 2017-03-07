362d3e47e6a23ec683394e13533703ffba221841
simon <crcms@crcms.cn>
2017-03-08 00:36:05 +0800

    listenr

---
+++ src/Listener/RepositoryListener.php
@@ -0,0 +1,61 @@
+<?php
+namespace CrCms\Repository\Listener;
+
+class RepositoryListener
+{
+
+    public function creating()
+    {
+
+    }
+
+
+    public function created()
+    {
+
+    }
+
+
+    public function deleting()
+    {
+
+    }
+
+
+    public function deleted()
+    {
+
+    }
+
+
+    public function updating()
+    {
+
+    }
+
+
+    public function updated()
+    {
+
+    }
+
+
+    public function saving()
+    {
+
+    }
+
+
+    public function saved()
+    {
+
+    }
+
+
+
+    public function handle()
+    {
+
+    }
+
+}
