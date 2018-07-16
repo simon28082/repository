89607e69e1ba0240dac6346405adc8f27e2ab457
crcms <crcms@crcms.cn>
2018-07-16 20:23:13 +0800

    Repository command generate

--- src/Console/Commands/Creator/RepositoryCreator.php
+++ src/Console/Commands/Creator/RepositoryCreator.php
@@ -11,15 +11,190 @@ use Illuminate\Filesystem\Filesystem;
  */
 class RepositoryCreator
 {
-    /**
-     * @var Filesystem|null
-     */
-    protected $fileSystem = null;
+//    /**
+//     * @var Filesystem
+//     */
+//    protected $fileSystem;
+//
+//    /**
+//     * @var Config
+//     */
+//    protected $config;
+//
+//    /**
+//     * @var string
+//     */
+//    protected $repository = '';
+//
+//    /**
+//     * @var string
+//     */
+//    protected $repositoryNameSpace = '';
+//
+//    /**
+//     * @var string
+//     */
+//    protected $model = '';
+//
+//    /**
+//     * RepositoryCreator constructor.
+//     * @param Filesystem $filesystem
+//     * @param Config $config
+//     */
+//    public function __construct(Filesystem $filesystem, Config $config)
+//    {
+//        $this->fileSystem = $filesystem;
+//        $this->config = $config;
+//    }
+//
+//    /**
+//     * @param string $repository
+//     * @return RepositoryCreator
+//     */
+//    public function setRepository(string $repository): self
+//    {
+//        //
+//        $repository = studly_case($repository);
+//
+//        //auto include namespace
+//        if (strpos($repository, '\\')) {
+//            $this->repositoryNameSpace = str_replace(strrchr($repository, '\\'), '', $repository);
+//            $repository = class_basename($repository);
+//        }
+//
+//        $this->repository = $repository;
+//
+//        return $this;
+//    }
+//
+//    /**
+//     * @param string $model
+//     * @return RepositoryCreator
+//     */
+//    public function setModel(string $model): self
+//    {
+//        $model = studly_case($model);
+//
+//        $this->model = $model;
+//
+//        return $this;
+//    }
+//
+//    /**
+//     * @param string $repository
+//     * @param string $model
+//     */
+//    public function create(string $repository, string $model = '')
+//    {
+//        //set and format arguments
+//        $this->setRepository($repository);
+//        $this->setModel($model);
+//
+//        if ($this->checkFileExists()) {
+//            throw new \Exception('repository file is exists');
+//        }
+//
+//        //create directory
+//        $this->createDirectory();
+//
+//        //get stub file content
+//        $content = $this->getFormatStubFileContent();
+//
+//        //write file
+//        $this->writeStubFile($content);
+//    }
+//
+//    /**
+//     *
+//     */
+//    protected function createDirectory()
+//    {
+//        $repositoryDirectory = $this->getRepositoryDirectoryPath();
+//
+//        if (!$this->fileSystem->isDirectory($repositoryDirectory)) {
+//            $this->fileSystem->makeDirectory($repositoryDirectory);
+//        }
+//    }
+//
+//    /**
+//     * @return string
+//     */
+//    protected function getRepositoryDirectoryPath(): string
+//    {
+//        return $this->repositoryNameSpace ?
+//            str_replace('\\', '/', $this->repositoryNameSpace) :
+//            $this->config->get('repository.repository_path');
+//    }
+//
+//    /**
+//     * @return string
+//     */
+//    protected function getRepositoryPath(): string
+//    {
+//        return $this->getRepositoryDirectoryPath() . '/' . $this->repository . '.php';
+//    }
+//
+//    /**
+//     * @return string
+//     */
+//    protected function getStubFilePath(): string
+//    {
+//        return __DIR__ . '/../../../../resource/stubs/repository.stub';
+//    }
+//
+//    /**
+//     * @return string
+//     */
+//    protected function getStubFileContent(): string
+//    {
+//        return $this->fileSystem->get($this->getStubFilePath());
+//    }
+//
+//    /**
+//     * @return string
+//     */
+//    protected function getFormatStubFileContent(): string
+//    {
+//        $repositoryNamespace = empty($this->repositoryNameSpace) ?
+//            $this->config->get('repository.repository_namespace') :
+//            $this->repositoryNameSpace;
+//
+//        return str_replace([
+//            'repository_namespace',
+//            'repository_class',
+//            'model_path',
+//        ], [
+//            $repositoryNamespace,
+//            $this->repository,
+//            $this->model,
+//        ], $this->getStubFileContent());
+//    }
+//
+//    /**
+//     * @param string $content
+//     */
+//    protected function writeStubFile(string $content)
+//    {
+//        $this->fileSystem->put($this->getRepositoryPath(), $content);
+//    }
+//
+//    /**
+//     * @return bool
+//     */
+//    protected function checkFileExists(): bool
+//    {
+//        return $this->fileSystem->exists($this->getRepositoryPath());
+//    }
 
     /**
-     * @var Config|null
+     * @var Filesystem
      */
-    protected $config = null;
+    protected $fileSystem;
+
+    /**
+     * @var Config
+     */
+    protected $config;
 
     /**
      * @var string
@@ -29,90 +204,87 @@ class RepositoryCreator
     /**
      * @var string
      */
-    protected $repositoryNameSpace = '';
+    protected $namespace = '';
 
     /**
      * @var string
      */
-    protected $model = '';
+    protected $path;
 
     /**
-     * RepositoryCreator constructor.
-     * @param Filesystem $filesystem
-     * @param Config $config
+     * @var string
      */
+    protected $model;
+
     public function __construct(Filesystem $filesystem, Config $config)
     {
         $this->fileSystem = $filesystem;
         $this->config = $config;
     }
 
-    /**
-     * @param string $repository
-     * @return RepositoryCreator
-     */
-    public function setRepository(string $repository): self
+    public function create(string $repository, string $model, string $path = '', string $namespace = '')
     {
-        //
-        $repository = studly_case($repository);
+        $this->setNamespace($namespace);
 
-        //auto include namespace
-        if (strpos($repository, '\\')) {
-            $this->repositoryNameSpace = str_replace(strrchr($repository, '\\'), '', $repository);
-            $repository = class_basename($repository);
-        }
-
-        $this->repository = $repository;
-
-        return $this;
-    }
-
-    /**
-     * @param string $model
-     * @return RepositoryCreator
-     */
-    public function setModel(string $model): self
-    {
-        $model = studly_case($model);
-
-        $this->model = $model;
-
-        return $this;
-    }
-
-    /**
-     * @param string $repository
-     * @param string $model
-     */
-    public function create(string $repository, string $model = '')
-    {
-        //set and format arguments
-        $this->setRepository($repository);
         $this->setModel($model);
 
-        if ($this->checkFileExists()) {
-            throw new \Exception('repository file is exists');
+        $this->setRepository($repository);
+
+        $this->setPath($path);
+
+        //check file exists
+        if ($this->fileSystem->exists($this->repositoryFilePath())) {
+            throw new Exception("File {$this->repositoryFilePath()} exists");
         }
 
         //create directory
-        $this->createDirectory();
-
-        //get stub file content
-        $content = $this->getFormatStubFileContent();
+        $this->autoCreateDirectory();
 
         //write file
-        $this->writeStubFile($content);
+        $this->writeStubFile($this->getFormatStubFileContent());
+    }
+
+    /**
+     * @param string $namespace
+     */
+    protected function setNamespace(string $namespace)
+    {
+        $this->namespace = $namespace ? $namespace : $this->config->get('repository.repository_namespace');
+    }
+
+    /**
+     * @param string $model
+     */
+    protected function setModel(string $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param string $magic
+     */
+    protected function setRepository(string $repository)
+    {
+        $this->repository = class_basename($repository);
+    }
+
+    /**
+     * @param string $path
+     */
+    protected function setPath(string $path)
+    {
+        $this->path = $path ? $path : $this->config->get('repository.repository_path');
     }
 
     /**
      *
      */
-    protected function createDirectory()
+    protected function autoCreateDirectory()
     {
-        $repositoryDirectory = $this->getRepositoryDirectoryPath();
+        $directory = $this->getRepositoryDirectoryPath();
 
-        if (!$this->fileSystem->isDirectory($repositoryDirectory)) {
-            $this->fileSystem->makeDirectory($repositoryDirectory);
+        if (!$this->fileSystem->isDirectory($directory)) {
+            $this->fileSystem->makeDirectory($directory, 0755, true);
         }
     }
 
@@ -121,17 +293,7 @@ class RepositoryCreator
      */
     protected function getRepositoryDirectoryPath(): string
     {
-        return $this->repositoryNameSpace ?
-            str_replace('\\', '/', $this->repositoryNameSpace) :
-            $this->config->get('repository.repository_path');
-    }
-
-    /**
-     * @return string
-     */
-    protected function getRepositoryPath(): string
-    {
-        return $this->getRepositoryDirectoryPath() . '/' . $this->repository . '.php';
+        return $this->path;
     }
 
     /**
@@ -155,18 +317,16 @@ class RepositoryCreator
      */
     protected function getFormatStubFileContent(): string
     {
-        $repositoryNamespace = empty($this->repositoryNameSpace) ?
-            $this->config->get('repository.repository_namespace') :
-            $this->repositoryNameSpace;
-
         return str_replace([
             'repository_namespace',
             'repository_class',
-            'model_path',
+            'model_namespace',
+            'model_class',
         ], [
-            $repositoryNamespace,
+            $this->namespace,
             $this->repository,
             $this->model,
+            class_basename($this->model),
         ], $this->getStubFileContent());
     }
 
@@ -175,15 +335,15 @@ class RepositoryCreator
      */
     protected function writeStubFile(string $content)
     {
-        $this->fileSystem->put($this->getRepositoryPath(), $content);
+        $this->fileSystem->put($this->repositoryFilePath(), $content);
     }
 
     /**
-     * @return bool
+     * @return string
      */
-    protected function checkFileExists(): bool
+    protected function repositoryFilePath(): string
     {
-        return $this->fileSystem->exists($this->getRepositoryPath());
+        return $this->getRepositoryDirectoryPath() . '/' . $this->repository . '.php';
     }
 }
 
--- src/Console/Commands/Repository.php
+++ src/Console/Commands/Repository.php
@@ -5,6 +5,7 @@ namespace CrCms\Repository\Console\Commands;
 use CrCms\Repository\Console\Commands\Creator\RepositoryCreator;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Class Repository
@@ -16,11 +17,6 @@ class Repository extends Command
      * @var string
      */
     protected $name = 'make:repository';
-
-    /**
-     * @var string
-     */
-    protected $signature = 'make:repository {repository} {--model=}';
 
     /**
      * @var string
@@ -52,14 +48,14 @@ class Repository extends Command
         $options = $this->options();
 
         if (empty($options['model'])) {
-            $this->error('You must enter an existing model');
+            $this->error('The model option not null');
             exit();
         }
 
         $this->creator->create($arguments['repository'], $options['model']);
 
         //update composer autoload
-        app('composer')->dumpAutoloads();
+        $this->getLaravel()->make('composer')->dumpAutoloads();
 
         $this->info("Successfully created the repository class");
     }
@@ -67,11 +63,20 @@ class Repository extends Command
     /**
      * @return array
      */
-    protected function getArguments()
+    protected function getArguments(): array
     {
         return [
-            ['repository', InputArgument::REQUIRED, 'The repository name.'],
-            ['model', InputArgument::REQUIRED, 'The model name.']
+            ['repository', InputArgument::REQUIRED, 'The repository name.']
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getOptions(): array
+    {
+        return [
+            ['model', null, InputOption::VALUE_REQUIRED, 'The model name.']
         ];
     }
 }
