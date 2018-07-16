2b03c5216608d86e2ed9008726d8a2094ca63e31
crcms <crcms@crcms.cn>
2018-07-17 06:43:13 +0800

    Remove redundant constraints

--- src/Contracts/RepositoryQueryRelate.php
+++ src/Contracts/RepositoryQueryRelate.php
@@ -10,20 +10,9 @@ namespace CrCms\Repository\Contracts;
 interface RepositoryQueryRelate
 {
     /**
-     * @param QueryRelate $queryRelate
-     * @return mixed
-     */
-//    public function setQueryRelate(QueryRelate $queryRelate);
-
-    /**
      * @return QueryRelate
      */
     public function getQueryRelate(): QueryRelate;
-
-    /**
-     * @return void
-     */
-    public function resetQueryRelate();
 
     /**
      * @return QueryRelate
