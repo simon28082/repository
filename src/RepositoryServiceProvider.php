24d032bd9a41c805873af42c26f4cf8b57dfa107
crcms <crcms@crcms.cn>
2018-08-12 15:53:32 +0800

    Defer load

--- src/RepositoryServiceProvider.php
+++ src/RepositoryServiceProvider.php
@@ -12,7 +12,7 @@ class RepositoryServiceProvider extends ServiceProvider
     /**
      * @var bool
      */
-    protected $defer = false;
+    protected $defer = true;
 
     /**
      * @var string
